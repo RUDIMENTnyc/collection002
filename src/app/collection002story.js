06cb41772c4ef73368723e5d1320610f64174cc7
al b <al.b@mac.com>
2018-02-14 23:16:52 -0500

    add para type 2

--- src/app/collection002story.js
+++ src/app/collection002story.js
@@ -4,7 +4,7 @@ function collection002storyController($log, $scope, $state) {
   $scope.$watch(function () {
     return $state.$current.name;
   }, function (newVal, oldVal) {
-    $log.info('yo');
+    $log.info('--');
   });
 }
 
